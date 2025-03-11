10752d52afd83a8231ea4576fd2c05e5e504d194
harsh456priyanshu <harshpriyanshu456@gmail.com>
2025-03-11 18:28:31 +0530

    Initial commit

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,9 @@
+const { model } = require("mongoose");
+
+const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+
+
+const HoldingsModel = new model("Holding", HoldingsSchema);
+
+
+module.exports = { HoldingsModel };
